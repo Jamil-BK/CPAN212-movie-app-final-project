fe32ecfb3dc34498bf7658a8aa73f94f36a291eb
Jamil Khalil <jamil7@dhaka.net>
2025-12-06 01:15:05 -0500

    Vercel serverless configuration

--- app.js
+++ app.js
@@ -43,14 +43,6 @@ app.get('/', (req, res) => {
 mongoose.connect(process.env.MONGO_URL)
 .then(() => {
   console.log('Connected to MongoDB');
-
-  // *** Required for Vercel deployment 
-
-  const PORT = process.env.PORT || 3000;
-
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
 })
 .catch(err => {
   console.error('MongoDB connection error:', err);
